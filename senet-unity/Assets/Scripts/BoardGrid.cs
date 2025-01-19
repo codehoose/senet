22db3d94d159560a8b3a5fba5e542b993ba6c797
Sloan Kelly <sloan@codehoose.com>
2025-01-19 13:14:02 +0000

    Added pawns to the board intro. Added a main menu / thumbnail for video scene.

--- senet-unity/Assets/Scripts/BoardGrid.cs
+++ senet-unity/Assets/Scripts/BoardGrid.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class BoardGrid : MonoBehaviour
@@ -11,13 +12,23 @@ public class BoardGrid : MonoBehaviour
     [SerializeField] private GameObject bottomLeftBlock;
     [SerializeField] private GameObject bottomRightBlock;
 
+    [SerializeField] private GameObject redPawn;
+    [SerializeField] private GameObject bluePawn;
+
+    [SerializeField] bool spawnPawns;
+
+    private List<Transform> blocks;
+
     private float gridSize = 1.02f;
 
     IEnumerator Start()
     {
+        blocks = new List<Transform>();
+
         float zDirection = 1f;
         float x = 0;
         float z = 0;
+        bool isRed = true;
 
         for (int i = 0; i < 30; i++)
         {
@@ -36,13 +47,39 @@ public class BoardGrid : MonoBehaviour
             go.transform.SetParent(transform);
             go.transform.position = Vector3.zero;
             go.transform.localPosition = new Vector3(x, 0, z);
+            blocks.Add(go.transform);
+            if (spawnPawns && i < 10)
+            {
+                var name = $"{(isRed ? "Red" : "Blue")}";
+                SpawnPawn(go.transform, isRed, name);
+                isRed = !isRed;
+            }
             yield return new WaitForSeconds(0.1f);
+        }
+    }
+
+    private void SpawnPawn(Transform t, bool isRed, string name)
+    {
+        var pawn = Instantiate(isRed ? redPawn : bluePawn, t);
+        pawn.name = name;
+        StartCoroutine(DropPawn(pawn, Vector3.up * 20f, Vector3.up * 0.093f));
+    }
+
+    IEnumerator DropPawn(GameObject go, Vector3 start, Vector3 end)
+    {
+        float time = 0f;
+        while (time < 1f)
+        {
+            var pos = Vector3.Lerp(start, end, time);
+            go.transform.localPosition = pos;
+            time += Time.deltaTime;
+            yield return null;
         }
     }
 
     private GameObject GetPieceFromIndex(int index)
     {
-        switch(index)
+        switch (index)
         {
             case 0: return Instantiate(startBlock);
             case 9: return Instantiate(topRightBlock);
@@ -53,5 +90,4 @@ public class BoardGrid : MonoBehaviour
             default: return Instantiate(straightBlock);
         }
     }
-
 }
