a39c2e2f2438ff8ac0ac10c6b1cc93ccd2b0d7aa
Sloan Kelly <sloan@codehoose.com>
2025-01-21 23:10:10 +0000

    Selection animation

--- senet-unity/Assets/Scripts/SelectionManager.cs
+++ senet-unity/Assets/Scripts/SelectionManager.cs
@@ -3,12 +3,47 @@ using UnityEngine.InputSystem;
 
 public class SelectionManager : MonoBehaviour
 {
-    [SerializeField] private Camera camera;
+    [SerializeField] private Camera mainCamera;
     [SerializeField] private GameObject selectionCursor;
+    [SerializeField] private bool selectionAllowed;
+    [SerializeField] private AnimationCurve scaleCurve;
+
+    private float scaleTime;
+
+    private enum SelectionState
+    {
+        None,
+        Hover,
+        Selected
+    }
+
+    private SelectionState state;
+
+    void Start()
+    {
+        state = SelectionState.None;
+    }
+
 
     // Update is called once per frame
     void Update()
     {
+        if (state == SelectionState.Selected)
+        {
+            var scale = scaleCurve.Evaluate(scaleTime);
+            selectionCursor.transform.localScale = new Vector3(scale, 1f, scale);
+            scaleTime += Time.deltaTime / 0.5f;
+            if (scaleTime > 1) scaleTime -= 1f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            selectionCursor.transform.localScale = new Vector3(1, 1, 1);
+            state = SelectionState.None;
+        }
+
+        if (!selectionAllowed || state == SelectionState.Selected) return;
+
         bool selectionCursorIsVisible = false;
         var ray = Camera.main.ScreenPointToRay(Mouse.current.position.value);
         RaycastHit hit;
@@ -22,6 +57,11 @@ public class SelectionManager : MonoBehaviour
                 selectionCursor.transform.position = transform.position + Vector3.up * 0.2f;
                 selectionCursor.transform.SetParent(transform);
                 selectionCursorIsVisible = true;
+
+                if (Input.GetMouseButtonDown(0))
+                {
+                    state = SelectionState.Selected;
+                }
             }
         }
 
